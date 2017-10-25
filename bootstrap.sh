933a2ccf2941ed8c3ab445da0124a80f22d4054b
rpowis <Rory Powis>
2017-10-25 20:06:14 +0200

    Don't copy brew.sh to /Users/rorypowis

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,6 +7,7 @@ git pull origin master;
 function doIt() {
 	rsync --exclude ".git/" \
 		--exclude ".DS_Store" \
+		--exclude "brew.sh" \
 		--exclude "bootstrap.sh" \
 		--exclude "README.md" \
 		--exclude "LICENSE-MIT.txt" \
