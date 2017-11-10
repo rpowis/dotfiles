f1d811ed8d87bd9b055c306378af8eb052fad919
Rory Powis <rory@hikkendry.com>
2017-11-10 10:36:30 +0100

    Don't install oh-my-zsh

--- bootstrap.sh
+++ bootstrap.sh
@@ -114,10 +114,6 @@ homebrew() {
 install_zsh () {
   # Test to see if zshell is installed.  If it is:
   if [ -f /bin/zsh -o -f /usr/bin/zsh ]; then
-    # Install Oh My Zsh if it isn't already present
-    if [[ ! -d $dir/oh-my-zsh/ ]]; then
-      sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-    fi
     # Set the default shell to zsh if it isn't currently set to zsh
     if [[ ! $(echo $SHELL) == $(which zsh) ]]; then
       chsh -s $(which zsh)
