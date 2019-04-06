edab28b5853ab036f3e707f9decd53ef3df57b80
Rory Powis <rorypowis@gmail.com>
2019-04-06 17:45:53 +0200

    Simplify bootstrap using ln flags

--- bootstrap.sh
+++ bootstrap.sh
@@ -22,11 +22,6 @@ ask_for_confirmation() {
   printf "\n"
 }
 
-execute() {
-  $1 &> /dev/null
-  print_result $? "${2:-$1}"
-}
-
 print_question() {
   # Print output in yellow
   printf "\e[0;33m  [?] $1\e[0m"
@@ -35,20 +30,6 @@ print_question() {
 print_error() {
   # Print output in red
   printf "\e[0;31m  [✖] $1 $2\e[0m\n"
-}
-
-print_success() {
-  # Print output in green
-  printf "\e[0;32m  [✔] $1\e[0m\n"
-}
-
-print_result() {
-  [ $1 -eq 0 ] \
-    && print_success "$2" \
-    || print_error "$2"
-
-  [ "$3" == "true" ] && [ $1 -ne 0 ] \
-    && exit
 }
 
 # ------------------------------------------------
@@ -60,7 +41,7 @@ print_result() {
 declare -a FILES_TO_SYMLINK=(
   shell/*
   git/*
-  apps/*
+  init
 )
 
 main() {
@@ -79,24 +60,10 @@ main() {
   local targetFile=''
 
   for i in ${FILES_TO_SYMLINK[@]}; do
-
     sourceFile="$(pwd)/$i"
     targetFile="$HOME/.$(printf "%s" "$i" | sed "s/.*\/\(.*\)/\1/g")"
 
-    if [ ! -e "$targetFile" ]; then
-      execute "ln -fs $sourceFile $targetFile" "$targetFile → $sourceFile"
-    elif [ "$(readlink "$targetFile")" == "$sourceFile" ]; then
-      print_success "$targetFile → $sourceFile"
-    else
-      ask_for_confirmation "'$targetFile' already exists, do you want to overwrite it?"
-      if answer_is_yes; then
-        rm -rf "$targetFile"
-        execute "ln -fs $sourceFile $targetFile" "$targetFile → $sourceFile"
-      else
-        print_error "$targetFile → $sourceFile"
-      fi
-    fi
-
+    ln -fsiv $sourceFile $targetFile
   done
 
   unset FILES_TO_SYMLINK
