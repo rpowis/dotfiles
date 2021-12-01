1c098ef7e177d742d612112fc355c6263732043c
Rory Powis <rpowis@users.noreply.github.com>
2021-12-01 20:58:02 +0100

    install: ignore immediate exit for checkout

--- bin/install.sh
+++ bin/install.sh
@@ -12,17 +12,16 @@ function dotfiles {
   git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
 }
 
-# 3. Checkout dotfiles to $HOME dir
-dotfiles checkout
+# 3. Checkout dotfiles to $HOME dir (bypassing `set -e` if the command fails)
+dotfiles checkout || true
 
 # 4. If checkout suceeded...
 if [ $? = 0 ]; then
   # 5. We're done!
   echo "Checked out dotfiles."
 else
-  # 6. Checkout again, backing up pre-existing dotfiles in the process
+  # 6. Checkout again, backing up pre-existing dotfiles in the
   mkdir -p .dotfiles.BAK
   echo "Backing up pre-existing dotfiles."
   dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .dotfiles.BAK/{}
-  echo "Checked out dotfiles."
 fi
