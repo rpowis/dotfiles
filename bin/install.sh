ee187be2de4463878f99772b3befa4e351a527c6
Rory Powis <rpowis@users.noreply.github.com>
2021-12-01 20:50:56 +0100

    install: checkout dotfiles and backup conflicts

--- bin/install.sh
+++ bin/install.sh
@@ -1,5 +1,28 @@
 #!/bin/bash
 
+# -e Exit immediately if a command fails
+# -u Treat unset variables as an error and exit immediately
+set -eu
+
 # 1. Clone as a bare repo
 git clone --bare https://github.com/rpowis/dotfiles.git $HOME/.dotfiles
 
+# 2. Set up an alias to make it easier to operate on the bare repo
+function dotfiles {
+  git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
+}
+
+# 3. Checkout dotfiles to $HOME dir
+dotfiles checkout
+
+# 4. If checkout suceeded...
+if [ $? = 0 ]; then
+  # 5. We're done!
+  echo "Checked out dotfiles."
+else
+  # 6. Checkout again, backing up pre-existing dotfiles in the process
+  mkdir -p .dotfiles.BAK
+  echo "Backing up pre-existing dotfiles."
+  dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .dotfiles.BAK/{}
+  echo "Checked out dotfiles."
+fi
