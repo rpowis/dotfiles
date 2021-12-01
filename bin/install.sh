c3e495e3b813d20a0f080603623c1806a43be432
Rory Powis <rpowis@users.noreply.github.com>
2021-12-01 21:28:46 +0100

    install: refactor checkout to handle errors only

--- bin/install.sh
+++ bin/install.sh
@@ -12,16 +12,16 @@ function dotfiles {
   git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
 }
 
-# 3. Checkout dotfiles to $HOME dir (bypassing `set -e` if the command fails)
-dotfiles checkout 2>&1 || true
+# 3. Checkout dotfiles to $HOME dir
+dotfiles checkout &>/dev/null
 
-# 4. If checkout suceeded...
-if [ $? = 0 ]; then
-  # 5. We're done!
-  echo "Checked out dotfiles."
-else
-  # 6. Checkout again, backing up pre-existing dotfiles in the
+# 4. If checkout failed...
+if [ $? -ne 0 ]; then
+  # 5. Checkout again, backing up pre-existing dotfiles in the process
   mkdir -p .dotfiles.BAK
   echo "Backing up pre-existing dotfiles."
   dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .dotfiles.BAK/{}
 fi
+
+# 7. We're done!
+echo "Checked out dotfiles."
